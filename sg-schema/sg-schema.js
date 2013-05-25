98fbe4e5b5082efd083a84cd9f201d1495b76554
David Tsuji <david@flodge.com>
2013-05-25 10:18:14 +1000

    sg-schema
    - completely rewritten and refactored
    - added a stack of tests

--- sg-schema/sg-schema.js
+++ sg-schema/sg-schema.js
@@ -1,257 +1,140 @@
 
-/*
-
-var myschema = {
-
-	name : 'string',
-	ismale : { _type : 'boolean', _default : true, _optional : true },
-	age : { _type : 'number' },
-	height : 'number',
-
-	address : {
-
-		street : { _type : 'string', _default : '1 smith st' },
-		postcode : { _type : 'number', _default : 4000, _optional : true },
-
-		work : {
-
-			suburb : 'string',
-			somearray : { _type : 'array', _values : [1,2,3] },
-			emptyobject : { _type : 'object', _values : [{a:'a'}, {b:'b'}] }
-
-		}
-
-	}
-
-}
-
-*/
-
 ;(function(){
 
-	var sgSchemaClass = function(){
-
-		this.typeRx        = /(string|number|boolean|array|object|date)/i;
-		this.privateKeysRx = /(_type|_optional|_default|_values|_pattern|_format)/i
-
-	}
+	var sgSchemaClass = function(){ }
 
 	sgSchemaClass.prototype = {
 
 		constructor: sgSchemaClass,
 
-		_parseSchemaProperties : function(_schema) {
+		cast : function(_value, _castType, _default, _values) {
+
+			var $this = this
+			  , parsedValue
+			  , valueType
+			  , value
+			  , values = typeof _values == 'object' && _values.constructor == Array ? _values : [];
+
+			if ( ! _.isUndefined(_value) && ! _.isNull(_value)) valueType = _value.constructor
+
+			if (valueType === _castType) {
+
+				value = _value;
+
+			} else {
+
+				switch(true) {
+
+					case _castType == Array && ! _.isNull(_value) && ! _.isUndefined(_value):
+
+						value = [_value];
+
+					break;
+
+					case _castType == Boolean:
+
+						try {
+
+							value = /^(true|1|y|yes)$/i.test(_value.toString()) ? true : undefined
+
+						}catch(e) {}
+
+						value = _.isBoolean(value) ? value : ( ! _.isUndefined(_default) && ! _.isNull(_default) ? _default : false )
+
+					break;
+
+					case _castType == Date:
+
+						try {
+
+							value = new Date(_value);
+							value = isNaN(value.getTime()) ? null : value;
+
+						} catch(e) {}
+
+					break;
+
+					case _castType == String:
+
+						try {
+
+							value = JSON.stringify(_value);
+							if ( _.isUndefined(value) ) throw '';
+
+						} catch(e) {
+
+							try { value = _value.toString() } catch(e){}
+
+						}
+
+					break;
+
+					default:
+
+						try { value = $this.cast(JSON.parse(_value), _castType) } catch(e) {}
+
+					break;
+
+				}
+
+			}
+
+			if (values.length > 0 && ! _.contains(values, value)) value = values[0];
+
+			return _.isUndefined(value) || _.isNull(value) || value == 'null' ? _default : value;
+
+		},
+
+		getSchemaProperties : function(_properties) {
 
 			var $this = this
 			  , properties
 
-			if (_.isString(_schema)) {
-
-				properties = {
-
-					_type     : $this.typeRx.test(_schema) ? _schema : 'string',
-					_optional : false,
-					_default  : '',
-					_values	  : [],
-					_format   : '',
-					
-				}
-
-			} else if (_.isObject(_schema)) {
-
-				properties = {
-
-					_type     : $this.typeRx.test(_schema['_type']) ? _schema['_type'] : 'object',
-					_optional : _.isBoolean(_schema['_optional']) ? _schema['_optional'] : false,
-					_default  : ! _.isUndefined(_schema['_default']) ? _schema['_default'] : null,
-					_format   : ! _.isUndefined(_schema['_format']) ? _schema['_format'] : '',
-					_values   : ! _.isUndefined(_schema['_values'])
-								? ( _.isString(_schema['_values'])
-										? [_schema['_values']]
-										: (_.isArray(_schema['_values']) ? _schema['_values'] : []) ) 
-								: []
-					
-				}
-
-			}
-
-			if (_.isUndefined(properties)) return;
-
-			if (properties._type == 'string') {
-
-				properties._default = _.isString(properties['_default'])
-									? properties['_default']
-									: properties['_values'][0] || ''
-
-			} else if (properties._type == 'number') {
-
-				properties._default = _.isNumber(properties['_default'])
-									? properties['_default']
-									: properties['_values'][0] || 0
-
-			} else if (properties._type == 'boolean') {
-
-				properties._default = _.isBoolean(properties['_default'])
-									? properties['_default']
-									: properties['_values'][0] || false
-
-			} else if (properties._type == 'array') {
-
-				properties._default = _.isArray(properties['_default'])
-									? properties['_default']
-									: (! _.isUndefined(properties['_values'][0]) ? [properties['_values'][0]] : [])
-
-			} else if (properties._type == 'object') {
-
-				properties._default = _.isObject(properties['_default']) && ! _.isEmpty(properties['_default'])
-									? properties['_default']
-									: properties['_values'][0] || {}
-
-			}
+			properties               = $this.cast(_properties, Object, { _type : _properties });
+			properties._type         = _.has(properties, '_type') ? properties._type : Object;
+			properties._optional     = $this.cast(properties['_optional'], Boolean, false);
+			properties._default      = properties['_default'];
+			properties._values       = $this.cast(properties['_values'], Array, []);
+			properties._typeAsString = $this.cast(properties._type, String, '').match(/^function ([^\(]*)\(\)/)[1];
+			properties._typeDefault  = $this.defaults[properties._typeAsString];
 
 			return properties;
 
 		},
 
-		_parseValue : function(_type, _value, _values, _format) {
-
-			if (_.isString(_value) || _.isNumber(_value)) {
-
-				if (_type == 'number') {
-
-					_value = _.isNaN(parseFloat(_value)) ? 0 : parseFloat(_value);
-
-				} else if (_type == 'string') {
-
-					try {
-
-						_value = _value.toString();
-
-					} catch(e) {
-
-						_value = null;
-
-					}
-
-				}  else if (_type == 'boolean') {
-
-					_value = /^(true|1|yes)$/i.test(_value)
-
-				} else if (_type == 'array') {
-
-					try {
-
-						_value = JSON.parse(_value);
-
-					} catch(e) {
-
-						_value = []
-
-					}
-					
-				} else if (_type == 'object') {
-
-					try {
-
-						_value = JSON.parse(_value);
-
-					} catch(e) {
-
-						_value = {}
-
-					}
-
-				} else if (_type == 'date') {
-
-					try {
-
-						var dte = moment(_value)
-
-						if (dte.isValid() == false) {
-
-							dte = moment(_value, 'DD/MM/YYYY')
-
-							if (dte.isValid() == false) throw Error('Cannot parse as moment date');
-
-						}
-
-						_value = dte.format(_format || '');
-
-					} catch(e) {
-
-						_value = new Date(_value);
-
-					}
-
-				}
-
-				if (_values.length > 0) _value = _.where(_values, _value).length > 0 ? _value : undefined;
-
-			}
-
-			if (_type == 'boolean') {
-
-				_value = _.isBoolean(_value) ? _value : _.isNumber(_value) && _value == 1
-
-			}
-
-			return _value;
-
-		},
-
-		_validateKeys : function(_result, _schema, _data) {
+		parseResult : function(_result, _data, _disableAutoDefauts) {
 
 			var $this = this
+			  , properties
+			  , objectData
+			  , defaultData
+			  , dataIsEmpty
 
-			_.each(_.keys(_schema), function(_key) {
+			_.each(_result, function(_properties, _key) {
 
-				var properties
+				properties = $this.getSchemaProperties(_properties);
 
-				if ($this.privateKeysRx.test(_key)) return;
+				objectData  = {};
+				defaultData = ! _.isUndefined(properties._default) || _disableAutoDefauts ? properties._default : properties._typeDefault;
+				dataIsEmpty = _data[_key];
 
-				properties = $this._parseSchemaProperties(_schema[_key]);
+				_.each(_.keys(properties), function(_key){
 
-				if ( ! _.isObject(properties)) return _result[_key] = _schema[_key];
+					if (/^[^_]/.test(_key)) objectData[_key] = properties[_key];
 
-				if (properties._type == 'object' && ! ( _.isObject(_data[_key]) &&  ! _.isArray(_data[_key]) ) ) {
+				});
 
-					if ( ! properties._optional || ! _.isUndefined(_data[_key])) {
+				if (properties._optional == false || (properties._optional == true && ! _.isUndefined(_data[_key]))) {
 
-						_data[_key]   = _.isObject(_data[_key]) && ! _.isEmpty(_data[_key]) ? _data[_key] : properties._default;
-						_result[_key] = $this._validateKeys(_result[_key] || {}, _schema[_key], _data[_key]);
+					_result[_key] = _.isEmpty(objectData)
+					              ? $this.cast(_data[_key], properties._type, defaultData, properties._values)
+					              : $this.parseResult(objectData, $this.cast(_data[_key], Object, {}), defaultData, _disableAutoDefauts);
 
-						if (_.isEmpty(_result[_key])) {
 
-							_result[_key] = properties._default;
+				} else {
 
-						}
+					delete _result[_key];
 
-					}
-
-				} else if ( ! properties._optional || ! _.isUndefined(_data[_key])) {
-
-					if (_.isObject(_data[_key]) && ! _.isArray(_data[_key])) {
-
-						_result[_key] = $this._validateKeys(_result[_key] || {}, _schema[_key], _data[_key]);
-
-					} else {
-
-						_data[_key] = $this._parseValue(properties._type, _data[_key], properties._values, properties._format);
-
-						if ( /date/.test(properties._type)) {
-
-							_result[_key] = _.isEmpty(_data[_key]) ? Date() : _data[_key];
-							
-						} else {
-
-							var type = _.isArray(_data[_key]) ? 'array' : typeof _data[_key];
-
-							_result[_key] = type == properties._type ? _data[_key] : properties._default;
-
-						}
-
-					}
-					
 				}
 
 			});
@@ -260,25 +143,12 @@ var myschema = {
 
 		},
 
-		apply : function(_schema, _data) {
+		apply : function(_schema, _data, _disableAutoDefauts) {
 
-			var $this = this;
+			var $this = this
+			  , schema = $.extend(true, {}, _schema || {})
 
-			if (_.isArray(_data)) {
-
-				_.each(_data, function(_value, _key){
-
-					_data[_key] = _.isEmpty(_schema) ? _value : $this._validateKeys({}, _schema, _value);
-
-				});
-				
-			} else {
-
-				_data = _.isEmpty(_schema) ? _data : $this._validateKeys({}, _schema, _data);
-
-			}
-
-			return _data;
+			return $this.parseResult(schema, _data, _disableAutoDefauts);
 
 		}
 
@@ -287,6 +157,15 @@ var myschema = {
 	window.sg = window['sg'] || {};
 	sg.schema = new sgSchemaClass();
 
-	sg.schema.defaults = { }
+	sg.schema.defaults = {
+
+		'Array'   : [],
+		'Boolean' : false,
+		'Date'    : new Date(),
+		'Number'  : 0,
+		'Object'  : {},
+		'String'  : '',
+
+	}
 
 })();
---
+++ sg-schema/tests/test.js
@@ -0,0 +1,256 @@
+
+
+test('cast test', function() {
+
+	var schema = {
+
+		aString  : String,
+		aNumber  : { _type : Number },
+		aBoolean : Boolean,
+		aArray   : Array,
+		aObject  : Object,
+
+	}
+	
+	var myBoolean = true
+	  , myArray = [1,2,3]
+	  , myString = 'test string'
+	  , myNumber = 1
+	  , myObject = {a: 'a'}
+	  , myDate = new Date()
+
+
+
+	ok( sg.schema.cast(myArray,   Array)   == myArray,   "myArray"   );
+	ok( sg.schema.cast(myBoolean, Boolean) == myBoolean, "myBoolean" );
+	ok( sg.schema.cast(myString,  String)  == myString,  "myString"  );
+	ok( sg.schema.cast(myNumber,  Number)  == myNumber,  "myNumber"  );
+	ok( sg.schema.cast(myObject,  Object)  == myObject,  "myObject"  );
+	ok( sg.schema.cast(myDate,    Date)    == myDate,    "myDate"    );
+
+
+	// # Array
+	// Cast from different types
+	ok( sg.schema.cast('1,2,3',   Array, [4,5,6]).constructor == Array,  "cast failed, so apply the default" );
+	ok( sg.schema.cast('[1,2,3]', Array, [4,5,6]).constructor == Array,  "cast failed, so apply the default" );
+
+
+	// # Boolean
+	// Cast from different types
+	ok( sg.schema.cast('true',    Boolean) == true,  "cast a String as a Boolean" );
+	ok( sg.schema.cast('y',       Boolean) == true,  "cast a String as a Boolean" );
+	ok( sg.schema.cast('yes',     Boolean) == true,  "cast a String as a Boolean" );
+	ok( sg.schema.cast(1,         Boolean) == true,  "cast a Number as a Boolean" );
+	ok( sg.schema.cast('n',       Boolean) == false, "cast a String as a Boolean" );
+	ok( sg.schema.cast(0,         Boolean) == false, "cast a Number as a Boolean" );
+	ok( sg.schema.cast(undefined, Boolean) == false, "cast a undefined as a Boolean" );
+	ok( sg.schema.cast(null,      Boolean) == false, "cast a null as a Boolean" );
+
+
+	// # Date
+	// Cast from different types
+	ok( sg.schema.cast('19 oct 1980', Date).toJSON() == '1980-10-18T14:00:00.000Z', "cast a String as a Date" );
+	ok( sg.schema.cast('1980-10-18T14:00:00.000Z', Date).constructor == Date, "cast a String as a Date" );
+	ok( sg.schema.cast('1980-10-18T14:00:00.000Z', Date).getTime() === 340725600000, "cast a String as a Date" );
+	ok( sg.schema.cast('', Date) == null, "cast a String as a Date" );
+	
+
+	// # Number
+	// Cast from different types
+	ok( sg.schema.cast('1',       Number, 0) == 1,  "cast a String as a Number" );
+	ok( sg.schema.cast('-11.5',   Number, 0) == -11.5,  "cast a String as a Number" );
+	ok( sg.schema.cast('+11.5',   Number, 2.3) == 2.3,  "cast a String as a Number" );
+	ok( sg.schema.cast('1+11.5',  Number) == undefined,  "cast a String as a Number" );
+
+
+	// # Object
+	// Cast from different types
+	ok( sg.schema.cast('{"a":{"b":"b"}}', Object).a.b === 'b',  "cast a String as an Object" );
+
+
+	// # String
+	// Cast from different types
+	ok( sg.schema.cast(12, String) === '12', "cast a String as an Object" );
+	ok( sg.schema.cast({a:'a'}, String) === '{"a":"a"}', "cast a String as an Object" );
+
+});
+
+test('schema test 1', function(){
+
+	var aSchema = {
+
+	 	name : { _type : String, _default : 'David' },
+	 	age : Number,
+	 	dob1 : Date,
+	 	dob2 : Date,
+	 	address : {
+
+	 		street : { _type : String, _default : '' },
+	 		postcode : { _type : Number, _default : '' },
+
+	 		work : {
+
+	 			country : String,
+
+	 		}
+
+	 	},
+
+	 	favouriteColor1 : { _type : String, _values : [ 'red', 'green', 'blue' ] },
+	 	favouriteColor2 : { _type : String, _values : [ 'red', 'green', 'blue' ], _default : 'orange' },
+	 	favouriteColor3 : { _type : String, _values : [], _default : 'orange' },
+
+	 	favouriteNumber1 : { _type : Number, _values : [ 1,2,3 ] },
+	 	favouriteNumber2 : { _type : Number, _values : [ 1,2,3 ] },
+	 	favouriteNumber3 : { _type : Number, _values : [ 1,2,3 ] },
+
+	 	likesPorkBelly1 : { _type : Boolean, _values : [ true ] },
+	 	likesPorkBelly2 : { _type : Boolean, _values : [ true ] },
+	 	likesPorkBelly3 : { _type : Boolean, _values : [ true ] },
+	 	
+
+	}
+
+	var anObject = {
+
+		age : 33,
+
+		dob1 : '19 Oct 1980',
+		dob2 : new Date(),
+
+		address : {
+
+			street : '1 smith street',
+			postcode : '4000',
+
+			work : {
+
+				badKey : 'this should not exist',
+
+			}
+
+		},
+
+		favouriteColor1 : 'black',
+		favouriteColor2 : 'black',
+		favouriteColor3 : null,
+		
+		favouriteNumber1 : 3,
+		favouriteNumber2 : 4,
+		favouriteNumber3 : '3',
+
+		likesPorkBelly1 : false,
+		likesPorkBelly2 : 'false',
+		likesPorkBelly3 : null,
+
+	};
+
+	var anObjectWithaSchemaApplied = sg.schema.apply(aSchema, anObject);
+	
+	ok( anObjectWithaSchemaApplied.name == 'David' );
+	ok( anObjectWithaSchemaApplied.age == 33, 'age should equal 33' );
+	ok( anObjectWithaSchemaApplied.dob1.getTime() == 340725600000, 'dob should equal 340725600000' );
+	ok( anObjectWithaSchemaApplied.dob2.getDate() == new Date().getDate(), 'dob should equal 340725600000' );
+	ok( anObjectWithaSchemaApplied.address.constructor == Object, 'address constructor equals an Object' );
+	ok( anObjectWithaSchemaApplied.address.street == '1 smith street', 'address.street equals a "1 smith street"' );
+	ok( anObjectWithaSchemaApplied.address.postcode == 4000, 'address.postcode equals 4000' );
+	ok( anObjectWithaSchemaApplied.address.work.constructor == Object, 'address.work constructor equals an Object' );
+	ok( anObjectWithaSchemaApplied.address.work.hasOwnProperty('badKey') == false, 'address.work does not contain a key "badKey"' );
+	ok( JSON.stringify(anObjectWithaSchemaApplied.address) == '{"street":"1 smith street","postcode":4000,"work":{"country":""}}', 'address stringified' );
+	ok( anObjectWithaSchemaApplied.favouriteColor1 == 'red', 'bad value so use the first value' );
+	ok( anObjectWithaSchemaApplied.favouriteColor2 == 'red', 'bad value but use the values instead of the default' );
+	ok( anObjectWithaSchemaApplied.favouriteColor3 == 'orange', 'bad value and no values so use the default' );
+
+	ok( anObjectWithaSchemaApplied.favouriteNumber1 == 3, 'more default value tests');
+	ok( anObjectWithaSchemaApplied.favouriteNumber2 == 1, 'more default value tests');
+	ok( anObjectWithaSchemaApplied.favouriteNumber3 == 3, 'more default value tests');
+
+	ok( anObjectWithaSchemaApplied.likesPorkBelly1 == true, 'more default value tests');
+	ok( anObjectWithaSchemaApplied.likesPorkBelly2 == true, 'more default value tests');
+	ok( anObjectWithaSchemaApplied.likesPorkBelly3 == true, 'more default value tests');
+
+});
+
+
+test('schema test 2', function(){
+
+	var aSchema = {
+
+		name : String,
+		ismale : { _type : Boolean, _default : true, _optional : true },
+		ispolitical : { _type : Boolean, _default : true, _optional : true },
+		age : { _type : Number },
+		height : Number,
+
+		address : {
+
+			street : { _type : String, _default : '1 smith st' },
+			postcode : { _type : Number, _default : 4000, _optional : true },
+
+			work : {
+
+				suburb : String,
+				somearray : { _type : Array, _values : [[1],[2],[3]] },
+				emptyobject : { _type : Object, _values : [{a:'a'}, {b:'b'}] }
+
+			}
+
+		}
+
+	}
+
+	var anObject = {
+
+		name : 'Max',
+		ismale : true,
+
+	}
+
+	var anObjectWithaSchemaApplied = sg.schema.apply(aSchema, anObject);
+
+	ok( JSON.stringify(anObjectWithaSchemaApplied) == '{"name":"Max","ismale":true,"age":0,"height":0,"address":{"street":"1 smith st","work":{"suburb":"","somearray":[1],"emptyobject":{"a":"a"}}}}' )
+
+});
+
+
+test('schema test with auto defaults off', function() {
+
+	var aSchema = {
+
+		name : String,
+		ismale : { _type : Boolean, _default : true, _optional : true },
+		ispolitical : { _type : Boolean, _default : true, _optional : true },
+		age : { _type : Number },
+		height : Number,
+
+		address : {
+
+			street : { _type : String, _default : '1 smith st' },
+			postcode : { _type : Number, _default : 4000, _optional : true },
+
+			work : {
+
+				suburb : String,
+				somearray : { _type : Array, _values : [[1],[2],[3]] },
+				emptyobject : { _type : Object, _values : [{a:'a'}, {b:'b'}] }
+
+			}
+
+		}
+
+	}
+
+	var anObject = {
+
+		name : 'Max',
+		ismaleBadKey : true,
+
+	}
+
+	var anObjectWithaSchemaApplied = sg.schema.apply(aSchema, anObject, true);
+
+	ok( JSON.stringify(anObjectWithaSchemaApplied) == '{"name":"Max","address":{"street":"1 smith st","work":{"somearray":[1],"emptyobject":{"a":"a"}}}}' );
+
+})
+
+
